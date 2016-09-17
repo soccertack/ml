8c74473fecbdd6eb854fca6dbf8964f04ffb1fbc
Jintack <jintack@cs.columbia.edu>
2016-09-17 10:15:04 -0400

    First working version

--- knn.py
+++ knn.py
@@ -6,9 +6,11 @@ import sys
 
 #TODO: return predicted labels preds
 def knn(training_data, training_labels, test_data, test_labels):
+	training_data = training_data.astype('float')
+	test_data = test_data.astype('float')
 	err = 0
+
 	for y in range (0, test_labels.size):
-	#for y in range (0, 10):
 		min_val = sys.maxint
 		min_label = 0
 		for x in range (0, training_labels.size):
@@ -17,22 +19,16 @@ def knn(training_data, training_labels, test_data, test_labels):
 				min_val = diff
 				min_label = training_labels[x]
 
-		ret = "[O]"
 		if min_label != test_labels[y]:
 			err += 1
-			ret = "[X]"
-		#print ret, "pred: ", min_label, "real: ", test_labels[y]
 	print err
 	return
 
-training_size = 8000
 ocr = loadmat('ocr.mat')
 #print ocr['data'].shape
-sel = random.sample(xrange(60000), training_size)
-'''
-print sel
-print ocr['data'][sel]
-print ocr['labels'][sel]
-'''
-#print "testdata size: ", ocr['testlabels'].size 
-knn(ocr['data'][sel], ocr['labels'][sel], ocr['testdata'], ocr['testlabels'])
+training_size = [1000, 2000, 4000, 8000]
+for i in range(len(training_size)):
+	for j in range(0, 10):
+		print j, "th run with size ", training_size[i]
+		sel = random.sample(xrange(60000), training_size[i])
+		knn(ocr['data'][sel], ocr['labels'][sel], ocr['testdata'], ocr['testlabels'])
