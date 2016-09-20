34bab80a4bfe162675ae54cb922b16f3eee7941e
Jintack <jintack@cs.columbia.edu>
2016-09-20 11:55:43 -0400

    Vectorize getting norm. Got 2x performance gain

--- knn.py
+++ knn.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python
 from scipy.io import loadmat
 import random
-import numpy
+import numpy as np
 import sys
 import matplotlib.pyplot as plt
 from matplotlib import cm 
 
-#TODO: return predicted labels preds
 def knn(training_data, training_labels, test_data, test_labels):
 	training_data = training_data.astype('float')
 	test_data = test_data.astype('float')
-	err = 0
+	knn_labels = []
 
 	for y in range (0, test_labels.size):
-		min_val = sys.maxint
-		min_label = 0
-		for x in range (0, training_labels.size):
-			diff = numpy.linalg.norm(abs(training_data[x] - test_data[y]))
-			if diff < min_val:
-				min_val = diff
-				min_label = training_labels[x]
+		diff = np.linalg.norm(training_data - test_data[y], axis=1)
+		#print min(diff)
+		#print np.argmin(diff)
+		knn_labels.append(training_labels[np.argmin(diff)])
 
-		if min_label != test_labels[y]:
+	return knn_labels
+
+def err_rate(test_labels,  knn_labels):
+	err = 0
+	for y in range (0, test_labels.size):
+		if test_labels[y] != knn_labels[y]:
 			err += 1
 	print err
-	return
+	return err
 
-def prototype_sel(training_data, training_labels):
+def prototype_sel(training_data, training_labels, m):
 
 	partitioned_data = [[] for i in range(10)]
 	label_mean = [[] for i in range(10)]
@@ -41,17 +42,21 @@ def prototype_sel(training_data, training_labels):
 
 	# Get average for each label
 	for i in range (0, 10):
-		label_mean[i] = numpy.mean(partitioned_data[i], axis=0)
+		label_mean[i] = np.mean(partitioned_data[i], axis=0)
+	
+	sel = random.sample(xrange(60000), 2*m)
+	# Get kNN from 2x input
 
 	return
 
 ocr = loadmat('ocr.mat')
 #print ocr['data'].shape
 #training_size = [1000, 2000, 4000, 8000]
-training_size = [1000]
+training_size = [2000]
 for i in range(len(training_size)):
 	for j in range(0, 10):
 		print j, "th run with size ", training_size[i]
 		sel = random.sample(xrange(60000), training_size[i])
-		prototype_sel(ocr['data'][sel], ocr['labels'][sel])
-	#	knn(ocr['data'][sel], ocr['labels'][sel], ocr['testdata'], ocr['testlabels'])
+	#	prototype_sel(ocr['data'][sel], ocr['labels'][sel], training_size)
+		knn_labels = knn(ocr['data'][sel], ocr['labels'][sel], ocr['testdata'], ocr['testlabels'])
+		err_rate(ocr['testlabels'], knn_labels)
