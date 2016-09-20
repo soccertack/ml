3b3685ad1c7241822b300fd646e3c8131a485afb
Jintack <jintack@cs.columbia.edu>
2016-09-19 23:23:09 -0400

    select prototype by getting kNN to average

--- knn.py
+++ knn.py
@@ -3,6 +3,8 @@ from scipy.io import loadmat
 import random
 import numpy
 import sys
+import matplotlib.pyplot as plt
+from matplotlib import cm 
 
 #TODO: return predicted labels preds
 def knn(training_data, training_labels, test_data, test_labels):
@@ -24,11 +26,32 @@ def knn(training_data, training_labels, test_data, test_labels):
 	print err
 	return
 
+def prototype_sel(training_data, training_labels):
+
+	partitioned_data = [[] for i in range(10)]
+	label_mean = [[] for i in range(10)]
+	partitioned_label = [0 for i in range(10)]
+	for d in range (0, training_labels.size):
+		idx = training_labels[d]
+		idx = idx[0]
+		partitioned_data[idx].append(training_data[d])
+		partitioned_label[idx] += 1
+
+	print partitioned_label[0]
+
+	# Get average for each label
+	for i in range (0, 10):
+		label_mean[i] = numpy.mean(partitioned_data[i], axis=0)
+
+	return
+
 ocr = loadmat('ocr.mat')
 #print ocr['data'].shape
-training_size = [1000, 2000, 4000, 8000]
+#training_size = [1000, 2000, 4000, 8000]
+training_size = [1000]
 for i in range(len(training_size)):
 	for j in range(0, 10):
 		print j, "th run with size ", training_size[i]
 		sel = random.sample(xrange(60000), training_size[i])
-		knn(ocr['data'][sel], ocr['labels'][sel], ocr['testdata'], ocr['testlabels'])
+		prototype_sel(ocr['data'][sel], ocr['labels'][sel])
+	#	knn(ocr['data'][sel], ocr['labels'][sel], ocr['testdata'], ocr['testlabels'])
