2be34f13394fe18d8829c715d8687f5b716b41a3
Jintack <jintack@cs.columbia.edu>
2016-09-21 19:33:40 -0400

    clean-up

--- knn.py
+++ knn.py
@@ -42,64 +42,23 @@ def err_rate(test_labels,  knn_labels):
 	return err_idx
 
 def prototype_sel(training_data, training_labels, m):
-
 	partitioned_data = [[] for i in range(10)]
-	label_mean = [[] for i in range(10)]
 	partitioned_label = [0 for i in range(10)]
 	selected_labels = [0 for i in range(m)]
+
 	for d in range (0, training_labels.size):
 		idx = training_labels[d]
 		idx = idx[0]
 		partitioned_data[idx].append(training_data[d])
 		partitioned_label[idx] += 1
 
-	#input_size = [m/100*5, m/100*2, m/10, m/100*13, m/100*14, m/10, m/100*4, m/10, m/100*20, m/100*12]
-	#input_size = [m/100*10, m/100*10, m/10, m/100*10, m/100*10, m/10, m/100*10, m/10, m/100*10, m/100*10]
-	#1000 11.225 0.299974998958
-	input_size = [m/100*10, 100, m/10, m/100*10, m/100*10, m/10, m/100*10, m/10, m/100*20 - 100, m/100*10]
-	'''
-	1000 11.215 0.362746467936
-	2000 8.395 0.354210389458
-	4000 6.28 0.135793961574
-	8000 5.104 0.14602739469
-	'''	
-
-	input_size = [m/100*5, 100, m/10, m/100*10, m/100*15, m/10, m/100*10, m/10, m/100*20 - 100, m/100*10]
-	'''
-1000 11.179 0.360678527223
-2000 8.264 0.24129649811
-4000 6.288 0.157594416145
-8000 4.934 0.183423008371
-	'''
-
-	input_size = [m/100*5, 100, m/10, m/100*10, m/100*15, m/10, m/100*8, m/10, m/100*20 - 100, m/100*12]
-	input_size = [m/100*5, m/100*2, m/10, m/100*13, m/100*14, m/10, m/100*4, m/10, m/100*20, m/100*12]
-	'''
-1000 10.748 0.365918023606
-2000 8.163 0.192252438216
-4000 6.325 0.23290556026
-8000 5.057 0.18831091312
-	'''
-	input_size = [m/100*4, m/100*2, m/100*11, m/100*13, m/100*14, m/10, m/100*4, m/10, m/100*20, m/100*12]
-	'''
-1000 10.574 0.478773432847
-2000 8.059 0.386094548006
-4000 6.26 0.128840987267
-8000 4.923 0.207704116473
-	'''
+	# pick number of prototypes for each labels
 	input_size = [m/100*4, m/1000*15, m/1000*115, m/100*13, m/100*14, m/10, m/100*4, m/10, m/100*20, m/100*12]
-	'''
-1000 10.667 0.559590028503
-2000 7.999 0.392796384912
-4000 6.286 0.181504820873
-8000 4.925 0.172872785597
-	'''
-
-
 	acc = 0
 	selected_labels = np.array(selected_labels)
+
+	# randomly pick prototype for each labels
 	for j in range(10):
-		#print "select ", input_size[j]
 		sel = random.sample(xrange(partitioned_label[j]), input_size[j]) 
 		data = np.array(partitioned_data[j])
 		if j==0:
@@ -112,42 +71,24 @@ def prototype_sel(training_data, training_labels, m):
 	return selected_data, selected_labels
 
 ocr = loadmat('ocr.mat')
-#print ocr['data'].shape
 training_size = [1000, 2000, 4000, 8000]
 iteration = 10
 use_proto = 1
-result = ["", "", "", ""]
+
 for i in range(len(training_size)):
 	err = []
-	err_array_total  = [[0 for u in range(10)] for w in range(10)]
 	for j in range(0, iteration):
 		sel = random.sample(xrange(60000), training_size[i])
 		t_data, t_label = ocr['data'][sel], ocr['labels'][sel]
 
-		#print type(ocr['data'])
 		if use_proto == 1:
 			t_data, t_label = prototype_sel(ocr['data'], ocr['labels'], training_size[i])
 		input_labels = ocr['testlabels']
-		#print type(t_data)
 		knn_labels = knn(t_data, t_label, ocr['testdata'], input_labels)
 		err_array = err_rate(input_labels, knn_labels)
-		err_array_total[j] = err_array
-	#	print err_array
-	#	print sum(err_array)
 		err.append(sum(err_array))
+
 	# At the end of iteratioin, print avg and stdev
 	div = input_labels.size/100	
-	result[i] = str(training_size[i]) + " " + str(np.mean(err)/div) + " " + str(np.std(err)/div)
-	err_array_total = np.array(err_array_total)
-	err_array_total = err_array_total.astype('float')
-	#print err_array_total
-	print training_size[i]
-	print np.sum(err_array_total, axis = 0)
-	print np.sum(err_array_total)
-	np.set_printoptions(formatter={'float': '{: 0.1f}'.format})
-	print np.sum(err_array_total, axis = 0)/np.sum(err)*100
-	#print np.sum(err, axis = 0)
-
-for i in range(4):
-	print result[i]
+	print training_size[i], np.mean(err)/div, np.std(err)/div
 
