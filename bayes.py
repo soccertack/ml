68eb118aaf055001ead150c09cbf0cb90895e9ea
Jintack <jintack@cs.columbia.edu>
2016-10-03 12:36:57 -0400

    class conditional distribution

--- bayes.py
+++ bayes.py
@@ -11,12 +11,7 @@ import time
 def partition(t_data, t_labels):
 
 	t_size = t_labels.size
-
 	class_size = np.unique(t_labels).size
-
-	# Test label 1 and 2 only 
-	t_size = 707
-	class_size = 2
 
 	partitioned_data = [[] for i in range(class_size)]
 	param = [0 for i in range(class_size)]
@@ -40,24 +35,23 @@ def get_class_prior(partitioned_data):
 			
 	print class_prior
 	print sum(class_prior)
-	return
-'''
-	print len(partitioned_data[0])
-	print [len(x) for x in partitioned_data]
-	print sum(len(x) for x in partitioned_data)
+	return class_prior
 
+def get_ccdist(partitioned_data):
+	class_size = len(partitioned_data)
+
+	ccdist = []
 	for y in range (class_size):
 		partitioned_data[y] = np.array(partitioned_data[y])
+		# sum for each xi where i is from 0 to d
 		column_sum = np.sum(partitioned_data[y], axis=0)
-		print column_sum.shape
-		print column_sum
 		column_sum = column_sum.todense()
-		column_sum += 1
-		column_sum /= (len(partitioned_data[y]) +2)
-		param[y] = column_sum
+		numerator = column_sum + 1
+		denominator = (len(partitioned_data[y]) +2)
+		ccdist.append(numerator/denominator)
 
-	print param
-'''
+	print ccdist
+	return ccdist
 
 news = loadmat('news.mat')
 # partitioned_data[i] contains data with label i
@@ -65,6 +59,7 @@ partitioned_data = partition(news['testdata'], news['testlabels'])
 print "class size is ", len(partitioned_data)
 
 class_prior = get_class_prior(partitioned_data)
+ccdist = get_ccdist(partitioned_data)
 
 
 sys.exit(0)
