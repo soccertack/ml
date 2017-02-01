3e22162679c51ecf483c160e0aba4fd4721b1f14
Jin Tack Lim <jintack@Jins-MBP.fios-router.home>
2017-02-01 03:15:14 -0500

    Use correct stdev. All configurations

--- FitCubic.py
+++ FitCubic.py
@@ -5,50 +5,48 @@ from LinearRegressionSimulator import *
 import matplotlib
 import pylab as pl
 import sys
+from matplotlib.backends.backend_pdf import PdfPages
 
 # my UNI is jl4312
-sim = LinearRegressionSimulator(np.array([[2, 1, 3, 4]]), 0)
+sim = LinearRegressionSimulator(np.array([[2, 1, 3, 4]]), 0.1)
 
 #http://mathworld.wolfram.com/LeastSquaresFittingPolynomial.html
 
-#Generate training and test set
-N=100
-M=10
-training = np.random.uniform(0, 1, N)
-test = np.random.uniform(0, 1, M)
-training.shape = (N, 1)
+runs = [(10, 10), (100, 10), (10, 100)]
+max_degree = 10
+for (M,N) in runs:
+	# Generate training data
+	training = np.random.uniform(0, 1, N)
+	training.shape = (N, 1)
+	XInput = pd.DataFrame(training)
+	training_y = (sim.SimPoly(XInput))
 
-#Set degree
-degree = 10
+	# Generate test data
+	test = np.random.uniform(0, 1, M)
+	test.shape = (N, 1)
+	XInput = pd.DataFrame(test)
+	test_y = (sim.SimPoly(XInput))
 
-x = np.vander(training.flatten(), degree+1, increasing=True)
-print (x)
+	for degree in range(0, max_degree+1):
+		x = np.vander(training.flatten(), degree+1, increasing=True)
 
-XInput = pd.DataFrame(training)
-y = (sim.SimPoly(XInput))
-print (y.T)
+		# a = (XTX)-1XTy
+		xtx_inv = inv(np.dot(x.T, x))
+		xtx_inv_xt = np.dot(xtx_inv, x.T)
+		a = np.dot(xtx_inv_xt, training_y.T)
 
-# Get coefficients
-print ("x shape: ", x.shape)
-xtx_inv = inv(np.dot(x.T, x))
-print ("xtx_inv shape: ", xtx_inv.shape)
-xtx_inv_xt = np.dot(xtx_inv, x.T)
-print ("xtx_inv_xt shape: ", xtx_inv_xt.shape)
-a = np.dot(xtx_inv_xt, y.T)
-print (a)
+		# Plot graph
+		reg_x = np.linspace(0, 1)
+		vand = np.vander(reg_x, degree+1, increasing=True).T
+		reg_y = np.dot(a.T, np.vander(reg_x, degree+1, increasing=True).T)
+		pl.plot(reg_x.flatten(), reg_y.flatten(), "g--")
 
-print ("a shape is ", a.shape)
-reg_x = np.linspace(0, 1)
-print ("reg_x shape is ", reg_x.shape)
-print (reg_x)
-vand = np.vander(reg_x, degree+1, increasing=True).T
-print ("vand shape: ", vand.shape)
-print (vand)
-reg_y = np.dot(a.T, np.vander(reg_x, degree+1, increasing=True).T)
-print ("xx shapte is ", reg_y.shape)
-print (reg_y)
-pl.plot(reg_x.flatten(), reg_y.flatten(), "b--")
+		# Plot Training data
+		pl.plot(training.flatten(), training_y.flatten(), 'r.')
+		# Plot Test data
+		pl.plot(test.flatten(), test_y.flatten(), 'b.')
 
-pl.plot(training.flatten(), y.flatten(), 'ro')
-pl.show()
-#compare output with numpy.polyfit
+		pl.show()
+
+	# TODO: remove
+	sys.exit()
