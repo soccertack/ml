6f463fa9bdad3fb5eee31aa4c0bfbd0535995f3b
Jin Tack Lim <jintack@Jins-MBP.fios-router.home>
2017-02-01 23:26:40 -0500

    Handle non-invertible matrix

--- FitCubic.py
+++ FitCubic.py
@@ -30,13 +30,33 @@ for index, (N,M) in enumerate(runs):
 
 	# Prepare pdf
 	pp = PdfPages('RiskPlot'+str(index)+'.pdf')
+	prev_R = 0
 	for degree in range(0, max_degree+1):
 		x = np.vander(training.flatten(), degree+1, increasing=True)
 
-		# a = (XTX)-1XTy
-		xtx_inv = inv(np.dot(x.T, x))
+		# Check if the XTX is invertible.
+		# http://stackoverflow.com/questions/13249108/efficient-pythonic-check-for-singular-matrix
+		if np.linalg.cond(np.dot(x.T, x)) < 1/sys.float_info.epsilon:
+			xtx_inv = inv(np.dot(x.T, x))
+			print("Invertible")
+		else:
+			xtx_inv = np.linalg.pinv(np.dot(x.T, x))
+			print("NOT Invertible")
+		# a = (XTX)-1XTy (Nx1 matrix)
 		xtx_inv_xt = np.dot(xtx_inv, x.T)
 		a = np.dot(xtx_inv_xt, training_y.T)
+
+		print ("degree: ", degree)
+		
+		diff  = training_y.T - np.dot(x, a)
+		R = np.linalg.norm(diff)**2
+		print ("R: ", R)
+
+		#print (diff)
+		if prev_R!=0 and (R > prev_R):
+			sys.exit()
+		prev_R = R
+			
 
 		# Plot graph
 		reg_x = np.linspace(0, 1)
@@ -49,6 +69,12 @@ for index, (N,M) in enumerate(runs):
 		# Plot Test data
 		pl.plot(test.flatten(), test_y.flatten(), 'b.')
 
+		'''
+		# This is for the verification of coefficient
+		z = np.polyfit(training.flatten(), training_y.flatten(), degree)
+		p = np.poly1d(z)
+		pl.plot(reg_x.flatten(), p(reg_x.flatten()), 'r-')
+		'''
 		#pl.show()
 		pp.savefig()
 		pl.close()
