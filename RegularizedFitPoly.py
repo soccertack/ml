60ebd7eb25926e5417619dcbe19c8a4a26e5e690
Jintack Lim <jintack@cs.columbia.edu>
2017-02-11 10:36:11 -0500

    Lamda selection done. Plot done. pickle to come

--- RegularizedFitPoly.py
+++ RegularizedFitPoly.py
@@ -1,18 +1,33 @@
 import numpy as np
 from numpy.linalg import inv
 import pandas as pd
-from LinearRegressionSimulator import *
+from HW1_sol import *
 import matplotlib
 import pylab as pl
 import sys
 from matplotlib.backends.backend_pdf import PdfPages
 
-'''
-1. Get sample from [-1, 1], not [0, 1]
-2. Use Legendre instead of simple power of x
-3. Add lambda for the regualized and set it to 0. We can just reuse FitCubic this case
-4. Pick lamda 2 to 5.
-'''
+# Generate K random inputs and K correspoding outputs from SimPoly function
+def gen_data(sim, K):
+	data_x = np.random.uniform(-1, 1, K)
+	data_x.shape = (K, 1)
+	XInput = pd.DataFrame(data_x)
+	data_y = (sim.SimPoly(XInput))
+	data_y.shape = (K, 1)
+	
+	return data_x, data_y
+
+# Computes the empirical risk of the given params Theta on the dataset
+def EmpiricalRisk(x, y, Theta, N):
+        return ( np.linalg.norm( y - np.dot( x, Theta ) ) ** 2 ) / (2. * N)
+
+def Plot_Original():
+	# my UNI is jl4312
+	sim_orig = LinearRegressionSimulator(np.array([2, 1, 3, 4]), 0)
+	orig_x = np.linspace(-1, 1)
+	XInput = pd.DataFrame(orig_x)
+	orig_y = (sim_orig.SimPoly(XInput))
+	pl.plot(orig_x, orig_y, label="original", color="black")
 
 def RegularizedFitPoly():
 	# my UNI is jl4312
@@ -23,70 +38,60 @@ def RegularizedFitPoly():
 	N = 10
 	M = 100
 
-	# Generate training data
-	training = np.random.uniform(-1, 1, N)
-	training.shape = (N, 1)
-	XInput = pd.DataFrame(training)
-	training_y = (sim.SimPoly(XInput))
-	training_y.shape = (N, 1)
+	training, training_y = gen_data(sim, N)
+	test, test_y = gen_data(sim, M)
+	# TODO: Save x and y for train and test to pickel file (problem1.pkl)
 
-	# Generate test data
-	test = np.random.uniform(-1, 1, M)
-	test.shape = (M, 1)
-	XInput = pd.DataFrame(test)
-	test_y = (sim.SimPoly(XInput))
-	test_y.shape = (M, 1)
+	# x: Nx7 array
+	# rows represent each input
+	# columns represent Legendre polinomial degree of i where i: [0, 6]
+	x = np.polynomial.legendre.legvander(training.flatten(), degree)
 
-	# Save x and y for train and test to pickel file (problem1.pkl)
-
-	risk_train = []
-	risk_test = []
-
-	# TODO: X is from legendre poly
-	x = np.vander(training.flatten(), degree+1, increasing=True)
-	
 	# Five lambda values
-	ld = [0, 0, 0, 0, 0]
-
-
-	# TODO: We need for-loop iterating ld[]
-	for idx, ld_val in enumerate(ld)
-	if ld_val  == 0:
-		# Use given solution for the linear regression
-	else:
-		# (X.T*X+lambda*N*I) is always invertible if lambda is not zero
-		xtx_inv = inv(np.dot(x.T, x)+ ld_val*N*np.identity(degree))
-
-		# thetaStar = (XTX)-1XTy (Nx1 matrix)
-		xtx_inv_xt = np.dot(xtx_inv, x.T)
-		thetaStar = np.dot(xtx_inv_xt, training_y)
-
-	# TODO: save thetaStar to pickle file
+	lambda_array = [0, 0.01, 0.1, 1, 10]
 	
-	# Get training risk
-	diff  = training_y - np.dot(x, a)
-	training_R = np.linalg.norm(diff)**2 / (2*N)
-	risk_train.append(training_R)
+	# Plot original and training data.
+	Plot_Original()
+	pl.plot(training, training_y, "ro",  label="Training data")
+	# Prepare for regression plots
+	cmap = pl.get_cmap('jet')
+	colors = cmap(np.linspace(0, 1, len(lambda_array)))
 
-	# Get test risk
-	# TODO: legendre
-	test_x = np.vander(test.flatten(), degree+1, increasing=True)
-	diff = test_y -np.dot(test_x, a)
-	test_R = np.linalg.norm(diff)**2 / (2*M)
-	risk_test.append(test_R)
+	# For each lamba, we have traning risk and test risk
+	risk_train = np.full( len(lambda_array) , fill_value=np.nan )
+	risk_test = np.full( len(lambda_array) , fill_value=np.nan )
 
-	np.savetxt("Risk.train."+str(run_index)+'.txt', np.array(risk_train))
-	np.savetxt("Risk.test."+str(run_index)+'.txt', np.array(risk_test))
+	# loop over different lambda values
+	for idx, lambda_val in enumerate(lambda_array):
+		if lambda_val == 0:
+			thetaStar = np.dot( np.linalg.pinv( x ), training_y )
+		else:
+			# (X.T*X+lambda*N*I) is always invertible if lambda is not zero
+			xtx_inv = inv(np.dot(x.T, x)+ lambda_val*N*np.identity(degree+1))
 
-	# Draw a plot
-	r_plot = PdfPages('RiskPlot'+str(run_index)+'.pdf')
-	deg = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-	pl.plot(deg, risk_train, "g--", label="Train Risk")
-	pl.plot(deg, risk_test, "r--", label="Test Risk")
+			# thetaStar = (XTX)-1XTy (Nx1 matrix)
+			xtx_inv_xt = np.dot(xtx_inv, x.T)
+			thetaStar = np.dot(xtx_inv_xt, training_y)
+
+		# TODO: save thetaStar to pickle file
+		
+		# Get training risk
+		risk_test[idx] = EmpiricalRisk(x, training_y, thetaStar, N)
+		# Get test risk
+		test_x = np.polynomial.legendre.legvander(test.flatten(), degree)
+		risk_test[idx] = EmpiricalRisk(test_x, test_y, thetaStar, M)
+
+		# Plot the regression with lambda
+		reg_x = np.linspace(-1, 1)
+		reg_x_x = np.polynomial.legendre.legvander(reg_x.flatten(), degree)
+		pl.plot(reg_x, np.dot( reg_x_x, thetaStar), \
+			label="lambda: "+str(lambda_val),color=colors[idx])
+		x1,x2,y1,y2 = pl.axis()
+		# Max y value of true data is 10 (4+3+1+2)
+		pl.axis((x1,x2,0,15))
+
 	pl.legend()
-	pl.xlabel('Dimension')
-	pl.xticks(np.linspace(0,10,11,endpoint=True))
-	r_plot.savefig()
-	pl.close()
-	r_plot.close()
+	pl.show()
+	# TODO: save risk 
 
+RegularizedFitPoly()
