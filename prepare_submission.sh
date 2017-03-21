86c1e4d1d489e4c0588d925a83c831269886acb1
Jintack Lim <jintack@cs.columbia.edu>
2017-03-20 20:05:07 -0400

    Clean up hw4

--- prepare_submission.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp hw3-2-sol.pdf submission/Assignment03_Problem02/Assignment03_Problem02.pdf
-cp *.py submission/Assignment03_Problem01/
-cp *.pkl submission/Assignment03_Problem01/
-cp Plot_* submission/Assignment03_Problem01/
-
