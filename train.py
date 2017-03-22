0d5ea0763b312e9df47bec8b3f67a72b9d768c6d
Jintack Lim <jintack@cs.columbia.edu>
2017-03-22 19:50:26 -0400

    Sample 1000 for each class. Remove outlier, Plot 10 in a pdf
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- train.py
+++ train.py
@@ -10,6 +10,9 @@ from sklearn import metrics
 from sklearn.model_selection import cross_val_score
 from matplotlib.backends.backend_pdf import PdfPages
 from timeit import default_timer as timer
+from sklearn import svm
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 INPUT_X_FILE="Data_x.pkl"
 INPUT_Y_FILE="Data_y.pkl"
@@ -19,8 +22,13 @@ def train(tr_x, tr_y):
 	print ("X data dimension is ", tr_x.shape)
 	print ("Y data dimension is ", tr_y.shape)
 
+	cov = np.cov(tr_x)
+	print ("cov")
+	print (cov)
 	# This is the line YOU need to play around
-	clf = LinearSVC()
+	clf = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
+                         algorithm="SAMME",
+                         n_estimators=200)
 
 	clf = clf.fit(tr_x, tr_y)
 	predicted_Y = clf.predict(tr_x)
@@ -53,22 +61,66 @@ f2.close()
 print (x_array.shape)
 print (y_array.shape)
 
-colors = ['dummy', 'blue', 'orange']
-for dims in itertools.combinations(range(100), 2):
+# TODO: remove this. Sample 1000 data
+sample_size = 500
+x_1 = x_array[0:sample_size]
+print ("x[0:sample]")
+print (x_1)
+y_1 = y_array[0:sample_size]
+x_2 = x_array[-sample_size:]
+y_2 = y_array[-sample_size:]
+x_array = np.concatenate((x_1, x_2), axis=0)
+y_array = np.concatenate((y_1, y_2), axis=0)
+print (x_array.shape)
+print (y_array.shape)
+print ("x[0:sample] + x[-sample:]")
+print (x_array)
+
+colors = ['dummy', 'blue', 'red']
+D = 100
+
+x_array= (x_array - x_array.mean()) / x_array.std()
+
+plt_idx = 0
+pic_num = 1
+for dims in itertools.combinations(range(D), 2):
 	dim1 = dims[0]
 	dim2 = dims[1]
+	plt_idx += 1
+	plt.subplot(2, 5, plt_idx)
 	for i in range (1,3):
 		# Pick rows with the given class i
 		X_x = x_array[y_array == i][:,dim1]
+		num_of_rows = X_x.shape[0]
 		X_y = x_array[y_array == i][:,dim2]
-		plt.scatter(X_x, X_y, marker='.', c=colors[i], s=1)
+
+		cond_array = X_x
+		orig_x = X_x
+		orig_y = X_y
+		X1 = orig_x[[np.all(cond_array[k] > -1) for k in range(0,num_of_rows)]]
+		Y1 = orig_y[[np.all(cond_array[k] > -1) for k in range(0,num_of_rows)]]
+
+		cond_array = Y1
+		orig_x = X1
+		orig_y = Y1
+		num_of_rows = Y1.shape[0]
+		X2 = orig_x[[np.all(cond_array[k] > -1) for k in range(0,num_of_rows)]]
+		Y2 = orig_y[[np.all(cond_array[k] > -1) for k in range(0,num_of_rows)]]
+
+		plt.scatter(X2, Y2, marker='.', c=colors[i], s=0.1)
 		plt.xlabel("Dimension "+str(dim1))
 		plt.ylabel("Dimension "+str(dim2))
-	plot = PdfPages("Plot_" + str(dim1) +"_" + str(dim2) +".pdf")
-	plot.savefig()
-	plot.close()
-	plt.close()
 
+	if (plt_idx == 10):
+		#plot = PdfPages("plots/Plot_" + str(dim1) +"_" + str(dim2) +".pdf")
+		plot = PdfPages("plots/" + str(pic_num) +".pdf")
+		plot.savefig()
+		plot.close()
+		plt.close()
+		pic_num += 1
+		plt_idx = 0
+
+sys.exit()
 train(x_array, y_array)
 predicted_Y = predict(x_array)
 print ("accuracy from dup: ", metrics.accuracy_score(y_array, predicted_Y))
