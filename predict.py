062a5449d3625f8d30fb6876ce497ab7e7766335
Jintack Lim <jintack@cs.columbia.edu>
2017-03-24 11:06:03 -0400

    Use the given format predict.py

--- predict.py
+++ predict.py
@@ -1,13 +1,33 @@
+##############################################
+# COMS 4771 Machine Learning Midterm
+# Name: Jin Tack Lim
+# UNI: jl4312
+##############################################
+
+# import all necessary modules here
+# see midterm instruction for requirements
+
 import numpy as np
 from sklearn.externals import joblib
 
 CLASSIFIER_FILE="Trained_classifier.pkl"
 
-def predict(test_x):
+def predict(X_test):
+	"""This function takes a dataset and predict the class label for each data point
+	should be in.
 
-	# Sample line to get the trained classifier
+	Parameters
+	----------
+	dataset: M X D numpy array
+		A dataset represented by numpy-array
+
+	Returns
+	-------
+	M x 1 numpy array
+		Returns a numpy array of predicted class labels
+	"""
+
 	clf = joblib.load(CLASSIFIER_FILE) 
-	test_y = clf.predict(test_x)
+	test_y = clf.predict(X_test)
 	return test_y 
-
 
