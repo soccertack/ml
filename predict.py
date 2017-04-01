71984f785e4ae33a0ba60506c4eb98c55de21b47
Jintack Lim <jintack@cs.columbia.edu>
2017-04-01 17:36:48 -0400

    Add basic files. Train with linear svm
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

---
+++ predict.py
@@ -0,0 +1,22 @@
+####################################################
+# COMS 4771 Machine Learning Assignment 5 Problem 2
+# Name:
+# UNI: 
+####################################################
+
+
+
+def predictSVM(X_test):
+	"""This function takes a dataset and predict the letter for each data point.
+
+	Parameters
+	----------
+	dataset: M X 128 numpy array
+		A dataset represented by numpy-array
+
+	Returns
+	-------
+	M x 1 numpy array
+		Returns a numpy array of letter that each data point represents
+	"""
+	# Your code here
